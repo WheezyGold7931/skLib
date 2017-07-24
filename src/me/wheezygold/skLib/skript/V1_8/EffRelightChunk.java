0e25649efba413ab0da3d4d55c9ac14151db8a12
WheezyGold7931 <joshroy126@gmail.com>
2017-07-24 19:54:27 -0400

    Add the Relight Chunk effect. (1.8)

---
+++ src/me/wheezygold/skLib/skript/V1_8/EffRelightChunk.java
@@ -0,0 +1,40 @@
+package me.wheezygold.skLib.skript.V1_8;
+
+import org.bukkit.event.Event;
+import org.eclipse.jdt.annotation.Nullable;
+
+import ch.njol.skript.Skript;
+import ch.njol.skript.lang.Effect;
+import ch.njol.skript.lang.Expression;
+import ch.njol.skript.lang.SkriptParser.ParseResult;
+import ch.njol.util.Kleenean;
+import net.minecraft.server.v1_8_R3.Chunk;
+
+public class EffRelightChunk extends Effect {
+	
+	static {
+		Skript.registerEffect(EffRelightChunk.class, 
+			"relight [chunk] %chunk%");
+	}
+	
+	private Expression<Chunk> cnk;
+
+	@SuppressWarnings("unchecked")
+	@Override
+	public boolean init(Expression<?>[] arg0, int arg1, Kleenean arg2, ParseResult arg3) {
+		cnk = (Expression<Chunk>) arg0[0];
+		return false;
+	}
+
+	@Override
+	public String toString(@Nullable Event arg0, boolean arg1) {
+		return null;
+	}
+
+	@Override
+	protected void execute(Event arg0) {
+		((Chunk) cnk).initLighting();
+		
+	}
+
+}
